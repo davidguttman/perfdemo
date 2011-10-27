98c95b60fd3be7ea318071182aec8ff07a7c7432
David Guttman <david@davidguttman.com>
2011-10-26 18:19:52 -0700

    animation loop working

--- public/js/script.js
+++ public/js/script.js
@@ -1,24 +1,37 @@
 (function() {
-  var create_canvas, initialize;
-  create_canvas = function(canvas_id) {
-    var canvas;
-    canvas = $("<canvas id='" + canvas_id + "' resize></canvas>");
-    canvas.appendTo($('body'));
-    canvas.width($(window).width());
-    canvas.height($(window).height());
-    return canvas[0];
-  };
-  initialize = function() {
-    var canvas, path, start;
-    canvas = create_canvas('paper_canvas');
-    paper.setup(canvas);
-    path = new paper.Path();
-    console.log("path", path);
-    path.strokeColor = 'black';
-    start = new paper.Point(100, 100);
-    path.moveTo(start);
-    path.lineTo(start.add([200, -50]));
-    return paper.view.draw();
-  };
-  $(document).ready(initialize);
+  var CoachPerf;
+  CoachPerf = (function() {
+    function CoachPerf(opts) {
+      this.canvas = this.create_canvas('paper_canvas');
+      paper.setup(this.canvas);
+      this.setup();
+      this.view = paper.view;
+      this.view.onFrame = this.draw;
+    }
+    CoachPerf.prototype.create_canvas = function(canvas_id) {
+      var canvas;
+      canvas = $("<canvas id='" + canvas_id + "' resize></canvas>");
+      canvas.appendTo($('body'));
+      canvas.width($(window).width());
+      canvas.height($(window).height());
+      return canvas[0];
+    };
+    CoachPerf.prototype.setup = function() {
+      var path, start;
+      path = new paper.Path();
+      console.log("path", path);
+      path.strokeColor = 'black';
+      start = new paper.Point(100, 100);
+      path.moveTo(start);
+      path.lineTo(start.add([200, -50]));
+      return paper.view.draw();
+    };
+    CoachPerf.prototype.draw = function(event) {
+      return console.log("event", event);
+    };
+    return CoachPerf;
+  })();
+  $(document).ready(function() {
+    return this.coach_perf = new CoachPerf();
+  });
 }).call(this);
